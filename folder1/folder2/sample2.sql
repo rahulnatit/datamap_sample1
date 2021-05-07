2dca46f76b076de7053f481f10e30a77633d5c79
rahul <rahul.prasad@cbnits.com>
2021-05-07 19:07:27 +0530

    second sql file added without

---
+++ folder1/folder2/sample2.sql
@@ -0,0 +1,8 @@
+SELECT
+  mysql.promethium.US_REGIONS.REGION_CODE AS REGION_CODE,
+  mysql.promethium.US_REGIONS.REGION_NAME AS REGION_NAME,
+  mysql.promethium.US_REGIONS.US_STATE AS US_STATE
+FROM
+  mysql.promethium.US_REGIONS
+LIMIT
+  10
